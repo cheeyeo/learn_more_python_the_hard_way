3b6bdc7b2c3c2be5c30e7fc385b8bf478cd70169
chee yeo <chee@chees-MacBook-Air.local>
2019-08-07 12:40:36 +0100

    Some fixes to code sample

--- chapter5_cat/cat.py
+++ chapter5_cat/cat.py
@@ -1,15 +1,12 @@
 import argparse
 
 ap = argparse.ArgumentParser(description="Implement the Linux cat command in python. Reads/concatenate files passed to it and prints it to stdout.")
-ap.add_argument("files", nargs="+")
+ap.add_argument("files", metavar='F', type=str, nargs="+")
 
 args = vars(ap.parse_args())
 
 files = args["files"]
 
-read_files = ""
 for file in files:
 	with open(file, 'r') as f:
-		read_files += f.read() + "\n"
-
-print(read_files)
+		print(f.read())
