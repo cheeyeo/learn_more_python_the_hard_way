2100f56c676705fc4a6810413c0d057c226d536f
chee yeo <ckyeo.1@gmail.com>
2019-08-13 11:44:10 +0100

    Add initial sed example

---
+++ chapter9_sed/sed.py
@@ -0,0 +1,26 @@
+import sys
+import re
+import string
+from utils import read_lines
+
+expression = sys.argv[1]
+targets = sys.argv[2:]
+
+# Check format of expression
+# If s/*** => search and replace
+search_replace = re.compile(r's\/.*\/.*\/g', re.I+re.S)
+res = search_replace.match(expression)
+if res != None:
+	expr = res.group()
+	expr = expr.split('/')
+	replacement = expr[1:3]
+else:
+	print('Expression is not defined. Only supports s/.*/g for now.')
+	exit(1)
+
+for target in targets:
+	lines = read_lines(target)
+	for line in lines:
+		# replace string with expression
+		new_line = re.sub(replacement[0], replacement[1], line)
+		print(new_line)
---
+++ chapter9_sed/utils.py
@@ -0,0 +1,3 @@
+def read_lines(file):
+	with open(file, 'r') as f:
+		return f.readlines()
