e5aa4ae7bdf808bc41ccc42051a6bbf8bc49af09
Chee Yeo <ckyeo.1@gmail.com>
2019-08-27 15:50:50 +0100

    Added numeric sort

--- chapter10_sort/README.md
+++ chapter10_sort/README.md
@@ -1,8 +1,32 @@
-# Sort program
+## Sort program
 
 Implementation of `sort` util in python.
 
 Exercise from chapter 10 of 'Learn You More Python The Hard Way'
+
+To run:
+```
+python sort.py [input file] [options]
+``` 
+
+To input from stdin:
+```
+ls | python sort.py [options]
+```
+
+To do numeric sort:
+```
+echo -e "1\n10\n11\n2\n3" | python sort.py -g
+```
+
+Options:
+```
+-r => Reverse sort
+
+-f => Ignore case sort
+
+-g => Numeric sort
+```
 
 
 ## References
--- chapter10_sort/sort.py
+++ chapter10_sort/sort.py
@@ -8,6 +8,7 @@ ap = argparse.ArgumentParser()
 ap.add_argument("file", nargs="?", help="File to sort on.")
 ap.add_argument("-r", "--reverse", action="store_true", help="Reverse sort.")
 ap.add_argument("-f", "--ignore_case", action="store_true", help="Ignore case.")
+ap.add_argument("-g", "--numeric_sort", action="store_true", help="Numeric sort.")
 args = vars(ap.parse_args())
 
 if has_stdin(sys.stdin):
@@ -28,6 +29,9 @@ case_opts = None
 if args["ignore_case"]:
 	case_opts = str.casefold
 
-sorted_lines = sorted(lines, key=str.casefold, reverse=args["reverse"])
+if args["numeric_sort"]:
+	case_opts = int
+
+sorted_lines = sorted(lines, key=case_opts, reverse=args["reverse"])
 for sl in sorted_lines:
 	print(sl.strip())
