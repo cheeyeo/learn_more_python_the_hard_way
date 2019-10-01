4d5e09359d7c5380c70840fb032065c5c9275ce0
Chee Yeo <ckyeo.1@gmail.com>
2019-10-01 16:14:37 +0100

    UPdates to chapter 14

--- chapter14_double_linked_lists/README.md
+++ chapter14_double_linked_lists/README.md
@@ -2,6 +2,16 @@
 
 Exercise from chapter 14 of 'More Learn Python The Hard Way'
 
+## Run unittest
+
+```
+ python test_dll.py
+
+ python -m unittest -v test_dll
+
+ python -m unittest discover -v
+```
+
 ## References
 
 * https://realpython.com/python-testing/#writing-your-first-test
--- chapter14_double_linked_lists/test_dll.py
+++ chapter14_double_linked_lists/test_dll.py
@@ -21,19 +21,120 @@ class TestDoubleLinkedList(TestCase):
 		self.assertEqual(dll.count(), 2)
 
 	def test_pop(self):
-		pass
+	  dll = DoubleLinkedList()
+	  self.assertIsNone(dll.pop())
+
+	  dll.push(1)
+	  self.assertEqual(dll.count(), 1)
+
+	  self.assertEqual(dll.pop(), 1)
+	  self.assertEqual(dll.count(), 0)
+	  self.assertIsNone(dll.begin)
+	  self.assertIsNone(dll.end)
+
+	  # Test case for more than 1 element
+	  dll.push(1)
+	  dll.push(2)
+	  self.assertEqual(dll.count(), 2)
+
+	  self.assertEqual(dll.pop(), 2)
+	  self.assertEqual(dll.count(), 1)
+	  self.assertEqual(dll.begin, dll.end)
+
+	  self.assertEqual(dll.pop(), 1)
+	  self.assertEqual(dll.count(), 0)
 
 	def test_shift(self):
-		pass
+		dll = DoubleLinkedList()
+		dll.shift(1)
+		self.assertEqual(dll.count(), 1)
+		self.assertEqual(dll.begin, dll.end)
+
+		dll.shift(2)
+		self.assertEqual(dll.count(), 2)
+		self.assertNotEqual(dll.begin, dll.end)
+		self.assertEqual(dll.begin.value, 2)
+		self.assertEqual(dll.end.value, 1)
 
 	def test_unshift(self):
-		pass
+		dll = DoubleLinkedList()
+		self.assertIsNone(dll.unshift())
+
+		dll.push(1)
+		self.assertEqual(dll.unshift(), 1)
+
+		dll.push(1)
+		dll.push(2)
+		self.assertEqual(dll.unshift(), 1)
+		self.assertEqual(dll.unshift(), 2)
 
 	def test_count(self):
-		pass
+		dll = DoubleLinkedList()
+		self.assertEqual(dll.count(), 0)
 
-	def test_detach_node(self):
-		pass
+		dll.push(1)
+		dll.push(2)
+		self.assertEqual(dll.count(), 2)
+
+	def test_remove(self):
+		colors = DoubleLinkedList()
+		colors.push("Cobalt")
+		colors.push("Zinc White")
+		colors.push("Nickle Yellow")
+		colors.push("Perinone")
+
+		# colors.dump("before removing cobalt")
+		self.assertEqual(colors.remove("Cobalt"), 0)
+
+		# colors.dump("before removing perinone")
+		self.assertEqual(colors.remove("Perinone"), 2)
+
+		# colors.dump("after removing perinone")
+
+		self.assertEqual(colors.remove("Nickle Yellow"), 1)
+		self.assertEqual(colors.remove("Zinc White"), 0)
+
+	def test_first(self):
+		dll = DoubleLinkedList()
+		dll.push(1)
+		self.assertEqual(dll.first(), 1)
+		dll.push(2)
+		self.assertEqual(dll.first(), 1)
+		dll.shift(-100)
+		self.assertEqual(dll.first(), -100)
+
+	def test_last(self):
+		dll = DoubleLinkedList()
+		dll.push(1)
+		self.assertEqual(dll.last(), 1)
+		dll.push(2)
+		self.assertEqual(dll.last(), 2)
+		dll.shift(-100)
+		self.assertEqual(dll.last(), 2)
+
+	def test_get(self):
+		colors = DoubleLinkedList()
+		colors.push("Vermillion")
+		self.assertEqual(colors.get(0), "Vermillion")
+
+		colors.push("Sap Green")
+		self.assertEqual(colors.get(0), "Vermillion")
+		self.assertEqual(colors.get(1), "Sap Green")
+
+		colors.push("Cadmium Yellow Light")
+		self.assertEqual(colors.get(0), "Vermillion")
+		self.assertEqual(colors.get(1), "Sap Green")
+		self.assertEqual(colors.get(2), "Cadmium Yellow Light")
+		self.assertEqual(colors.pop(), "Cadmium Yellow Light")
+
+		self.assertEqual(colors.get(0), "Vermillion")
+		self.assertEqual(colors.get(1), "Sap Green")
+		self.assertEqual(colors.get(2), None)
+		colors.pop()
+		self.assertEqual(colors.get(0), "Vermillion")
+		colors.pop()
+		self.assertEqual(colors.get(0), None)
+
 
 if __name__ == '__main__':
 	unittest.main()
